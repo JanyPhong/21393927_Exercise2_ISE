2a0cd35cbab2fbb40ce8782663c44e5d87d6ff90
JanyPhong <135357650+JanyPhong@users.noreply.github.com>
2023-06-03 01:50:54 +0800

    Solution for Question A

--- Q4.py
+++ Q4.py
@@ -1,19 +1,6 @@
-'''
-This program implements the Pocket Beast game. The game is played by two players.
-'''
-
-
 import random
 
 def create_beasts():
-    '''
-    This function creates the beasts for the players.
-    '''
-    global player_one_health
-    global player_two_health   
-    global player_one_beast
-    global player_two_beast 
-      
     player_one_health = 50
     player_two_health = 50
 
@@ -23,12 +10,10 @@ def create_beasts():
     print("Player 1's beast is a " + player_one_beast)
     print("Player 2's beast is a " + player_two_beast)
 
+    return player_one_health, player_two_health, player_one_beast, player_two_beast
+
 
 def attack(dice_type, dice_number, is_defending):
-    '''
-    This function simulates an attack by rolling the dice and returning the total 
-    unless the beast is defending then it does nothing
-    '''
     total = 0
     if is_defending:
         print("Defending!")
@@ -39,17 +24,13 @@ def attack(dice_type, dice_number, is_defending):
 
 
 def print_attack_message(player_one_beast, player_one_attack, player_two_beast, player_two_attack, current_player):
-    '''
-    This function prints the attack message for the provided player
-    '''
     if current_player == 1:
         print(f"{player_one_beast} attacks {player_two_beast} for {player_one_attack} damage")
     elif current_player == 2:
         print(f"{player_two_beast} attacks {player_one_beast} for {player_two_attack} damage")
 
-def play_game():
-    global player_one_health
-    global player_two_health
+
+def play_game(player_one_health, player_two_health, player_one_beast, player_two_beast):
     while player_one_health > 0 and player_two_health > 0:
         print(f"{player_one_beast}'s health: {player_one_health}")
         print(f"{player_two_beast}'s health: {player_two_health}")
@@ -70,10 +51,10 @@ def play_game():
         print_attack_message(player_one_beast, player_one_attack, player_two_beast, player_two_attack, 1)
         print_attack_message(player_one_beast, player_one_attack, player_two_beast, player_two_attack, 2)
 
-def print_winner():
-    '''
-    This function prints the winner of the game
-    '''
+    return player_one_health, player_two_health
+
+
+def print_winner(player_one_health, player_one_beast, player_two_beast):
     if player_one_health <= 0:
         print(f"{player_two_beast} wins!")
     else:
@@ -81,6 +62,6 @@ def print_winner():
 
 
 if __name__ == "__main__":
-    create_beasts()
-    play_game()
-    print_winner()
+    player_one_health, player_two_health, player_one_beast, player_two_beast = create_beasts()
+    player_one_health, player_two_health = play_game(player_one_health, player_two_health, player_one_beast, player_two_beast)
+    print_winner(player_one_health, player_one_beast, player_two_beast)
