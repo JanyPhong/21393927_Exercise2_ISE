60e91694ea67ca5e0384604890cd57079a7e7863
JanyPhong <135357650+JanyPhong@users.noreply.github.com>
2023-06-03 01:53:39 +0800

    Solution for Question B

--- Q4.py
+++ Q4.py
@@ -9,10 +9,6 @@ def create_beasts():
     '''
     This function creates the beasts for the players.
     '''
-    global player_one_health
-    global player_two_health   
-    global player_one_beast
-    global player_two_beast 
       
     player_one_health = 50
     player_two_health = 50
@@ -23,64 +19,48 @@ def create_beasts():
     print("Player 1's beast is a " + player_one_beast)
     print("Player 2's beast is a " + player_two_beast)
 
+    return player_one_health, player_two_health, player_one_beast, player_two_beast
 
-def attack(dice_type, dice_number, is_defending):
-    '''
-    This function simulates an attack by rolling the dice and returning the total 
-    unless the beast is defending then it does nothing
-    '''
+def roll_dice(dice_type, dice_number):
     total = 0
-    if is_defending:
-        print("Defending!")
-    else:
-        for _ in range(dice_number):
-            total = total + random.randint(1, dice_type)
+    for _ in range(dice_number):
+        total += random.randint(1, dice_type)
     return total
 
 
-def print_attack_message(player_one_beast, player_one_attack, player_two_beast, player_two_attack, current_player):
-    '''
-    This function prints the attack message for the provided player
-    '''
-    if current_player == 1:
-        print(f"{player_one_beast} attacks {player_two_beast} for {player_one_attack} damage")
-    elif current_player == 2:
-        print(f"{player_two_beast} attacks {player_one_beast} for {player_two_attack} damage")
+def perform_attack(attacker_beast, defender_beast, attacker_defending):
+    if attacker_defending:
+        print(f"{attacker_beast} is defending!")
+    else:
+        attacker_attack = roll_dice(6, 2)
+        print(f"{attacker_beast} attacks {defender_beast} for {attacker_attack} damage")
+        return attacker_attack
 
 def play_game():
-    global player_one_health
-    global player_two_health
+    player_one_health, player_two_health, player_one_beast, player_two_beast = create_beasts()
     while player_one_health > 0 and player_two_health > 0:
         print(f"{player_one_beast}'s health: {player_one_health}")
         print(f"{player_two_beast}'s health: {player_two_health}")
-        player_one_defend = input("Player 1, do you want to defend? (y/n): ")
-        player_two_defend = input("Player 2, do you want to defend? (y/n): ")
-        if player_one_defend == "y":
-            player_one_attack = attack(6, 2, True)
-        else:
-            player_one_attack = attack(6, 2, False)
-
-        if player_two_defend == "y":
-            player_two_attack = attack(6, 2, True)
-        else:
-            player_two_attack = attack(6, 2, False)
         
-        player_one_health = player_one_health - player_two_attack
-        player_two_health = player_two_health - player_one_attack
-        print_attack_message(player_one_beast, player_one_attack, player_two_beast, player_two_attack, 1)
-        print_attack_message(player_one_beast, player_one_attack, player_two_beast, player_two_attack, 2)
+        player_one_defend = input("Player 1, do you want to defend? (y/n): ")
+        player_one_attack = perform_attack(player_one_beast, player_two_beast, player_one_defend == "y")
+        
+        player_two_defend = input("Player 2, do you want to defend? (y/n): ")
+        player_two_attack = perform_attack(player_two_beast, player_one_beast, player_two_defend == "y")
+        
+        player_one_health -= player_two_attack
+        player_two_health -= player_one_attack
 
-def print_winner():
-    '''
-    This function prints the winner of the game
-    '''
+    return player_one_health, player_two_health, player_one_beast, player_two_beast
+
+
+def print_winner(player_one_health, player_two_beast):
     if player_one_health <= 0:
         print(f"{player_two_beast} wins!")
     else:
-        print(f"{player_one_beast} wins!")
+        print("Player 1 wins!")
 
 
 if __name__ == "__main__":
-    create_beasts()
-    play_game()
-    print_winner()
+    player_one_health, player_two_health, player_one_beast, player_two_beast = play_game()
+    print_winner(player_one_health, player_two_beast)
